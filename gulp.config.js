4f7bb4b9300c2bd715fc2a2407ba8b0448a49964
Chris Podmore <podmorec@adastra.co.uk>
2016-06-30 13:47:56 +0100

    Compile Less to CSS; autoPrefixer

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,16 @@
 module.exports = function (){
+    var client = './src/client/';
+
     var config = {
+        temp: './.tmp/',
 
         // all the js I want to vet
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+
+        less: client + 'styles/styles.less'
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
-
+var del = require('del');
 var $ = require('gulp-load-plugins')({ lazy: true });
 
 gulp.task('vet', function () {
@@ -14,6 +14,32 @@ gulp.task('vet', function () {
         .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
         .pipe($.jshint.reporter('fail'));
 });
+
+gulp.task('styles', ['clean-styles'], function () {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({ browsers: ['last 2 versions', '> 5%'] }))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function () {
+    var files = config.temp + '**/*.css';
+    clean(files);
+});
+
+gulp.task('less-watcher', function () {
+    gulp.watch([config.less], ['styles']);
+});
+
+////////////////////
+function clean(path) {
+    log('Cleaning: ' + $.util.colors.yellow(path));
+    del(path);
+}
 
 function log(message) {
     if (typeof (message) === 'object') {
